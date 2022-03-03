695b603861db91868102645745600c487bf9353e
shiroha <keqing.hu@icloud.com>
2022-03-03 12:53:46 +0800

    feat: 完成榜单

--- src/pages/contest/Rank.vue
+++ src/pages/contest/Rank.vue
@@ -1,20 +1,138 @@
 <template>
-    <div style="margin-top: 60px">
-        非常抱歉，这个模块正在开发中，敬请等待<br>
-        Sorry, this module is under development, please wait<br>
-        申し訳ありませんが、このモジュールは開発中です。しばらくお待ちください<br>
-        죄송합니다. 이 모듈은 개발 중입니다. 잠시만 기다려 주십시오.<br>
-        Désolé, ce module est en cours de développement, veuillez patienter<br>
-        ขออภัย โมดูลนี้อยู่ระหว่างการพัฒนา กรุณารอสักครู่<br>
-        Entschuldigung, dieses Modul befindet sich in der Entwicklung, bitte warten<br>
-        Извините, этот модуль находится в разработке, пожалуйста, подождите<br>
-        На жаль, гэты модуль знаходзіцца ў стадыі распрацоўкі, пачакайце<br>
+    <div style="width: 100%; overflow: auto">
+        <div v-if="metaData">
+            <h3>我</h3>
+            <Table :head="tableHead" :data="metaData">
+                <template v-slot:body-totalCost="scope">
+                    {{`${(scope.data.totalCost / 3600).toFixed(0).padStart(2, '0')}:${((scope.data.totalCost % 3600) / 60).toFixed(0).padStart(2, '0')}:${(scope.data.totalCost % 60).toFixed(0).padStart(2, '0')}`}}
+                </template>
+            </Table>
+        </div>
+        <h3>总榜单</h3>
+        <Table :head="tableHead" :data="tableData">
+            <template v-slot:body-totalCost="scope">
+                {{`${(scope.data.totalCost / 3600).toFixed(0).padStart(2, '0')}:${((scope.data.totalCost % 3600) / 60).toFixed(0).padStart(2, '0')}:${(scope.data.totalCost % 60).toFixed(0).padStart(2, '0')}`}}
+            </template>
+        </Table>
+        <Pagination @change="changePageNum" v-model="pageNum" :total="totalNum" :pageSize="pageSize"></Pagination>
     </div>
 </template>
 
 <script>
 export default {
-    name: "Rank"
+    name: "Rank",
+    data() {
+        return {
+            contestId: parseInt(this.$route.params.contestId),
+            contestData: null,
+            rankList: null,
+            myRank: null,
+            pageNum: 1,
+            pageSize: 30,
+            totalNum: 0,
+            totalPage: 0,
+
+            tableHead: [],
+            tableData: [],
+            metaData: [],
+        }
+    },
+    created() {
+        this.pageNum = this.$common.getQueryInt(this, 'pageNum', 1)
+        this.pageSize = this.$common.getQueryInt(this, 'pageSize', 30)
+        this.$contest.getContestData(this.contestId, res => {
+            this.contestData = res
+            this.tableHead = [{
+                label: '排名',
+                value: 'rank',
+                width: '30',
+            }, {
+                label: '昵称',
+                value: 'handle',
+                width: '50',
+            }, {
+                label: '解决',
+                value: 'totalSolve',
+                width: '20',
+            }, {
+                label: '罚时',
+                value: 'totalCost',
+                width: '50',
+            }]
+            for (let i = 0; i < res.problemList.length; i++) {
+                this.tableHead.push({
+                    label: i,
+                    value: `p${i}`,
+                })
+            }
+        })
+        this.initData()
+    },
+    methods: {
+        initData() {
+            let query = {
+                pageNum: this.pageNum.toString(),
+                pageSize: this.pageSize.toString()
+            }
+            if (JSON.stringify(this.$route.query) !== JSON.stringify(query)) {
+                this.$router.push({
+                    name: 'contest-rank',
+                    query: query
+                })
+            }
+            this.$contest.rank(this.contestId, this.pageNum, this.pageSize, res => {
+                this.pageNum = res.pageNum
+                this.pageSize = res.pageSize
+                this.totalNum = res.totalNum
+                this.totalPage = res.totalPage
+                this.tableData = []
+                for (let i = 0; i < res.data.length; i++) {
+                    this.tableData.push(this.reduceData(res.data[i]))
+                }
+                if (res.metaData) {
+                    this.metaData = [this.reduceData(res.metaData)];
+                } else {
+                    this.metaData = null
+                }
+            })
+        },
+        reduceData(data) {
+            let res = {
+                rank: data.rank + 1,
+                handle: data.userHandle,
+                totalSolve: data.totalSolve,
+                totalCost: data.totalCost,
+            }
+            // noinspection JSUnresolvedVariable
+            for (const submitStatusKey in data.submitStatus) {
+                // noinspection JSUnresolvedVariable
+                const tmp = data.submitStatus[submitStatusKey]
+                let value = ''
+                // noinspection JSUnresolvedVariable
+                if (tmp.firstSolveId) {
+                    // noinspection JSUnresolvedVariable
+                    value += `${(tmp.solveTime / 3600).toFixed(0).padStart(2, '0')}:`
+                    // noinspection JSUnresolvedVariable
+                    value += `${((tmp.solveTime % 3600) / 60).toFixed(0).padStart(2, '0')}:`
+                    // noinspection JSUnresolvedVariable
+                    value += `${(tmp.solveTime % 60).toFixed(0).padStart(2, '0')}`
+                }
+                // noinspection JSUnresolvedVariable
+                if (tmp.penaltyTimes && tmp.penaltyTimes !== 0) {
+                    value += `(${tmp.penaltyTimes})`
+                }
+                // noinspection JSUnresolvedVariable
+                if (tmp.runningSubmit && tmp.runningSubmit !== 0) {
+                    value += `+${tmp.runningSubmit}`
+                }
+                res[`p${submitStatusKey}`] = value;
+            }
+            return res
+        },
+        changePageNum() {
+            this.initData()
+        },
+    }
 }
 </script>
 
